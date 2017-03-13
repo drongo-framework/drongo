207369cc837e7087c83169c9d9fbd0cd9116b48e
Sattvik Chakravarthy <sattvik@gmail.com>
2017-03-13 22:23:47 +0530

    Adding conversion functions for dict2.

--- drongo/utils.py
+++ drongo/utils.py
@@ -4,3 +4,36 @@ class dict2(dict):
 
     def __getattr__(self, name):
         return self.setdefault(name, dict2())
+
+    @classmethod
+    def from_dict(cls, val):
+        if isinstance(val, dict):
+            res = cls()
+            for k, v in val.items():
+                res[k] = cls.from_dict(v)
+            return res
+        elif isinstance(val, list):
+            res = []
+            for item in val:
+                res.append(cls.from_dict(item))
+            return res
+        else:
+            return val
+
+    def to_dict(self, val=None):
+        val = val or self
+        if isinstance(val, dict2):
+            res = dict()
+            for k, v in val.items():
+                res[k] = self.to_dict(v)
+            return res
+        elif isinstance(val, list):
+            res = []
+            for item in val:
+                res.append(self.to_dict(item))
+            return res
+        else:
+            return val
+
+    def __repr__(self):
+        return 'dict2(%s)' % super(dict2, self).__repr__()
