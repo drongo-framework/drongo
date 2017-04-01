b70d659e702f70fe5801dce7f0cd73c6d662602d
Sattvik Chakravarthy <sattvik@gmail.com>
2017-04-01 11:17:28 +0530

    Adding support for processing POST query.

--- drongo/request.py
+++ drongo/request.py
@@ -18,10 +18,24 @@ class Request(object):
         inp = env.get('wsgi.input')
         self._query = urllib.parse.parse_qs(env.setdefault('QUERY_STRING', ''))
 
+        if self.method == 'POST':
+            self.process_post()
+
         # Load the cookies
         self._cookies = dict2()
         for cookie in http.cookies.BaseCookie(env.get('HTTP_COOKIE')).values():
             self._cookies[cookie.key] = cookie.value
+
+    def process_post(self):
+        content_type = self.env['CONTENT_TYPE']
+        content_length = int(self.env['CONTENT_LENGTH'])
+        if content_type == 'application/x-www-form-urlencoded':
+            input = self.env['wsgi.input']
+            post_data = b''
+            while len(post_data) < content_length:
+                post_data += input.read(content_length - len(post_data))
+            post_query = urllib.parse.parse_qs(post_data.decode('utf8'))
+            self._query.update(post_query)
 
     def process_files(self):
         fs = cgi.FieldStorage(inp, environ=env)
