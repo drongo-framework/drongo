49218b6bdb78ee297c191f689643c17720a6b443
Sattvik Chakravarthy <sattvik@gmail.com>
2017-05-12 08:32:40 +0530

    Refactoring url matching into separate class.

--- drongo/app.py
+++ drongo/app.py
@@ -8,10 +8,66 @@ import sys
 import traceback
 
 
+class UrlManager(object):
+    def __init__(self):
+        self._routes = {}
+
+    def add_url(self, pattern, method=None, call=None):
+        if not pattern.endswith('/'):
+            pattern += '/'
+        parts = tuple(pattern.split('/')[1:])
+        node = self._routes
+        for part in parts:
+            node = node.setdefault(part, {})
+        if method is None:
+            node['GET'] = call
+        elif isinstance(method, str):
+            node[method] = call
+        else:
+            for m in method:
+                node[m] = call
+
+    def find_call(self, path, method):
+        if not path.endswith('/'):
+            path += '/'
+        path = path.split('/')[1:]
+        return self._recursive_route_match(self._routes, path, method, [])
+
+    def _recursive_route_match(self, node, remaining, method, args):
+        # Route is stored in tree form for quick matching compared to
+        # traditional form of regular expression matching
+        if len(remaining) == 0:
+            if callable(node.get(method)):
+                return (node.get(method), args)
+            else:
+                return None
+
+        result = None
+        for key in node:
+            if key == remaining[0]:
+                result = self._recursive_route_match(node[key], remaining[1:],
+                                                     method, args)
+                if result:
+                    return result
+            elif len(key) and key[0] == '{':
+                # We match for concrete part first and then compare
+                # parameterised parts.
+                continue
+        for key in node:
+            if len(key) and key[0] == '{':
+                result = self._recursive_route_match(
+                    node[key], remaining[1:], method,
+                    args + [(key[1:-1], remaining[0])]
+                )
+                if result:
+                    return result
+        return None
+
+
 class Drongo(object):
     def __init__(self):
-        self.routes = {}
         self.context = dict2()
+        self._url_manager = UrlManager()
         self._middlewares = []
         self._logger = logging.getLogger('drongo')
 
@@ -28,9 +84,14 @@ class Drongo(object):
         response = ctx.response = Response()
 
         # Route matching
-        match = self.match_route(request.path, request.method)
+        match = self._url_manager.find_call(request.path, request.method)
+
         if match:
             meth, args = match
+            ctx.callable = meth
+            ctx.callable_args = args
+
+            # Convert tuple to dictionary
             args = {k: v for k, v in args}
             try:
                 for mw in self._middlewares:
@@ -49,16 +110,19 @@ class Drongo(object):
             except Exception as e:
                 response.set_status(HttpStatusCodes.HTTP_500)
                 response.set_content('Internal server error!')
+
                 for mw in self._middlewares:
                     if hasattr(mw, 'exception'):
                         mw.exception(ctx)
                 exc_type, exc_value, exc_traceback = sys.exc_info()
+
                 self._logger.error('\n'.join(traceback.format_exception(
                     exc_type, exc_value, exc_traceback)))
 
         else:
             response.set_status(HttpStatusCodes.HTTP_404)
             response.set_content('Not found!')
+
         # Returns empty response in case of no match
         self._logger.info('{method}\t{path}\t{status}'.format(
             method=request.method, path=request.path,
@@ -67,58 +131,6 @@ class Drongo(object):
 
     def url(self, pattern, method=None):
         def _inner(call):
-            self.add_url(pattern, method, call)
+            self._url_manager.add_url(pattern, method, call)
             return call
         return _inner
-
-    def add_url(self, pattern, method=None, call=None):
-        if not pattern.endswith('/'):
-            pattern += '/'
-        parts = tuple(pattern.split('/')[1:])
-        node = self.routes
-        for part in parts:
-            node = node.setdefault(part, {})
-        if method is None:
-            node['GET'] = call
-            node['POST'] = call
-        elif isinstance(method, str):
-            node[method] = call
-        else:
-            for m in method:
-                node[m] = call
-
-    def recursive_route_match(self, node, remaining, method, args):
-        # Route is stored in tree form for quick matching compared to
-        # traditional form of regular expression matching
-        if len(remaining) == 0:
-            if callable(node.get(method)):
-                return (node.get(method), args)
-            else:
-                return None
-
-        result = None
-        for key in node:
-            if key == remaining[0]:
-                result = self.recursive_route_match(node[key], remaining[1:],
-                                                    method, args)
-                if result:
-                    return result
-            elif len(key) and key[0] == '{':
-                # We match for concrete part first and then compare
-                # parameterised parts.
-                continue
-        for key in node:
-            if len(key) and key[0] == '{':
-                result = self.recursive_route_match(
-                    node[key], remaining[1:], method,
-                    args + [(key[1:-1], remaining[0])]
-                )
-                if result:
-                    return result
-        return None
-
-    def match_route(self, path, method):
-        if not path.endswith('/'):
-            path += '/'
-        path = path.split('/')[1:]
-        return self.recursive_route_match(self.routes, path, method, [])
