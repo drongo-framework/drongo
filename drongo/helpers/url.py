b87938076d52cbdc0423d47d574b66f86b9f4880
Sattvik Chakravarthy <sattvik@gmail.com>
2017-09-06 11:03:34 +0800

    Adding doc for url helper.

--- drongo/helpers/url.py
+++ drongo/helpers/url.py
@@ -1,6 +1,23 @@
 class URLHelper(object):
+    """Helper class with using urls within the class"""
+
     @classmethod
     def url(cls, pattern, method='GET'):
+        """Decorator to mark url pattern for a callable
+
+        Args:
+            pattern (:obj:`str`): URL pattern to add. This is usually '/'
+                separated path. Parts of the URL can be parameterised using
+                curly braces.
+                Examples: "/", "/path/to/resource", "/resoures/{param}"
+            method (:obj:`str`, :obj:`list` of :obj:`str`, optional): HTTP
+                methods for the path specied. By default, GET method is added.
+                Value can be either a single method, by passing a string, or
+                multiple methods, by passing a list of strings.
+
+        Note:
+            A trailing '/' is always assumed in the pattern.
+        """
         def _inner(func):
             setattr(func, '__drongo_url_pattern', pattern)
             setattr(func, '__drongo_url_method', method)
@@ -10,6 +27,13 @@ class URLHelper(object):
 
     @classmethod
     def mount(cls, app, instance, base_url=''):
+        """Mount an instance with methods marked with url on to a drongo app.
+
+        Args:
+            app: Drongo app instance.
+            instance: Instance that contains methods with url decorator.
+            base_url: URL prefix for all the url patterns used.
+        """
         for attr in dir(instance):
             attr = getattr(instance, attr)
             if hasattr(attr, '__drongo_url_pattern'):
