e1c4d61b94ac491fa456e4d591191044d27614af
Sattvik Chakravarthy <sattvik@gmail.com>
2017-10-12 13:13:14 +0530

    Adding tests for middleware.

--- tests/test_app.py
+++ tests/test_app.py
@@ -36,3 +36,40 @@ class TestApp(unittest.TestCase):
         env['PATH_INFO'] = '/error'
         result = list(self.app(env, start_response))
         self.assertEqual(b'Internal server error!', b''.join(result))
+
+    def test_middleware(self):
+        def start_response(status_code, headers):
+            pass
+
+        class MyMiddleware(object):
+            def before(self, ctx):
+                pass
+
+            def after(self, ctx):
+                pass
+
+            def exception(self, ctx, exc):
+                pass
+
+        self.app.add_middleware(MyMiddleware())
+
+        @self.app.url('/home')
+        def home(ctx):
+            return ctx.request.query.get('hello')[0]
+
+        @self.app.url('/error')
+        def error(ctx):
+            raise Exception('Testing error')
+
+        env = dict(
+            REQUEST_METHOD='GET',
+            GET=dict(hello=['world']),
+            PATH_INFO='/home',
+            HTTP_COOKIE='a=b'
+        )
+        result = list(self.app(env, start_response))
+        self.assertEqual(b'world', b''.join(result))
+
+        env['PATH_INFO'] = '/error'
+        result = list(self.app(env, start_response))
+        self.assertEqual(b'Internal server error!', b''.join(result))
